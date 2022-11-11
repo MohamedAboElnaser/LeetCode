aae0bddc5a276a284074f332fb5dd442dfa955bd
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2022-11-11 18:23:56 +0200

    solution of Divisor Game problem

---
+++ _1025_Divisor_Game.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    bool divisorGame(int n) {
+       if(n%2==0) return true;
+       else return false; 
+    }
+};
+int main(){
+    
+    return 0;
+}
