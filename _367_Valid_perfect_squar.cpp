f805bd220877787ecec1f88c949d12a57ace3323
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2022-12-15 21:19:31 +0200

    solution of Valid prfect squar

---
+++ _367_Valid_perfect_squar.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+class Solution {
+public:
+    bool isPerfectSquare(int num) {
+        for(int i=0;i*i>=0&&i*i<=num;i++)
+        {
+            if(i*i==num)return true;
+        }
+        return false;
+    }
+     
+};
+int main(){
+    Solution s;
+    cout<<s.isPerfectSquare(14);
+    return 0;
+}
