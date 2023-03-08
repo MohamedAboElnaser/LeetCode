d7ca0c95cd2dbd30bdf865938073f434db422100
Mohamed_aboelnasr <moohamedaboelnasr@outlook.com>
2023-03-08 17:50:07 +0200

    add solution of 1502. Can Make Arithmetic Progression From Sequence

---
+++ source/_502_Can Make Arithmetic Progression From Sequence.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+       if(arr.size()==2)return true;
+        sort(arr.begin(),arr.end());
+       int dif=arr[1]-arr[0];
+       for(int i=2;i<arr.size();i++){
+           if((arr[i]-arr[i-1])!=dif)return false;
+       } 
+       return true;
+    }
+};
+
+
+
+
+int main(){
+
+    return 0;
+
+}
